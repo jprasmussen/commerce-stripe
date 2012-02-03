53db327073b8d799468f46cf16d088c296b6e1b0
Ilari Mäkelä <ilari.makela@mearra.com>
2012-02-03 16:36:20 +0200

    A lot of new functionality originally created by [jsmerth].

--- commerce_stripe.js
+++ commerce_stripe.js
@@ -1,44 +1,49 @@
-(function ($) {
 
-Drupal.behaviors.stripeConfig = {
-  attach: function (context) {
-    // For now we'll just do the test form.
-    $("#stripe-form").submit(function(event) {
-        // disable the submit button to prevent repeated clicks
-        $('.submit-button').attr("disabled", "disabled");
+jQuery(document).ready(function() {
 
-        var amount = $('.amount').val();
-        Stripe.createToken({
-            number: $('.card-number').val(),
-            cvc: $('.card-cvc').val(),
-            exp_month: $('.card-expiry-month').val(),
-            exp_year: $('.card-expiry-year').val()
-        }, amount, stripeResponseHandler);
+  jQuery("#commerce-checkout-form-review").submit(function(event) {
+    
+    // prevent the form from submitting with the default action
+    event.preventDefault();
+    
+    // show progress animated gif (needed for submitting after first error)
+    jQuery('.checkout-processing').show();
+    
+    // disable the submit button to prevent repeated clicks
+    jQuery('.form-submit').attr("disabled", "disabled");
 
-        // prevent the form from submitting with the default action
-        return false;
-    });
+    var amount = jQuery('.stripe-order-total').val();
+    Stripe.createToken({
+        number: jQuery('#edit-commerce-payment-payment-details-credit-card-number').val(),
+        cvc: jQuery('#edit-commerce-payment-payment-details-credit-card-code').val(),
+        exp_month: jQuery('#edit-commerce-payment-payment-details-credit-card-exp-month').val(),
+        exp_year: jQuery('#edit-commerce-payment-payment-details-credit-card-exp-year').val()
+    }, amount, stripeResponseHandler);
+
+    // prevent the form from submitting with the default action
+    return false;
+  });
+  
+});
+
+function stripeResponseHandler(status, response) {
+
+  if (response.error) {
+    //show the errors on the form
+    jQuery("div.payment-errors").html(jQuery("<div class='messages error'></div>").html(response.error.message));
+    
+    // enable the submit button to allow resubmission
+    jQuery('.form-submit').removeAttr("disabled");
+    // hide progress animated gif
+    jQuery('.checkout-processing').hide();
   }
-};
-
-
-
-  stripeResponseHandler = function(status, response) {
-    console.log(response);
-    if (response.error) {
-      //show the errors on the form
-      $("#stripe-form").after($('<div class="payment-errors"></div>').html(response.error.message));
-    }
-    else {
-      var $form = $("#stripe-form");
-      // token contains id, last4, and card type
-      var token = response['id'];
-      // insert the token into the form so it gets submitted to the server
-      $form.append("<input type='hidden' name='stripeToken' value='" + token + "'/>");
-      // and submit
-      $form.get(0).submit();
-      alert('done');
-    }
+  else {
+    var form$ = jQuery("#commerce-checkout-form-review");
+    // token contains id, last4, and card type
+    var token = response['id'];
+    // insert the token into the form so it gets submitted to the server
+    form$.append("<input type='hidden' name='stripeToken' value='" + token + "'/>");
+    // and submit
+    form$.get(0).submit();
   }
-
-})(jQuery);
+}
